a4d82497c662246a1970121d2ae6345594d834d3
Felipe Cardozo <fraifelipe@gmail.com>
2022-12-29 09:35:11 -0300

    fix typo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	middlewares "github.com/marigold-dev/tzproxy/middlawares"
+	middlewares "github.com/marigold-dev/tzproxy/middlewares"
 	utils "github.com/marigold-dev/tzproxy/utils"
 	"github.com/rs/zerolog"
 	"github.com/ulule/limiter/v3/drivers/store/memory"
