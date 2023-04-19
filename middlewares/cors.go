a6faf57db06c11b47904c6d88dad8a288173c548
Felipe Cardozo <fraifelipe@gmail.com>
2023-04-19 19:41:05 -0300

    set cors without libs

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,26 +1,19 @@
 package middlewares
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	utils "github.com/marigold-dev/tzproxy/utils"
 )
 
 func CORS(config *utils.Config) echo.MiddlewareFunc {
-	if config.CORSEnabled {
-		return middleware.CORSWithConfig(middleware.CORSConfig{
-			AllowOrigins:     []string{"*"},
-			AllowMethods:     []string{http.MethodGet, http.MethodOptions, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-			AllowHeaders:     []string{echo.HeaderContentType},
-			MaxAge:           172800,
-			AllowCredentials: true,
-		})
-	}
-
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			if config.CORSEnabled {
+				allowOriginHeader := c.Request().Header.Get("Access-Control-Allow-Origin")
+				if allowOriginHeader == "" {
+					c.Request().Header.Set("Access-Control-Allow-Origin", "*")
+				}
+			}
 			return next(c)
 		}
 	}
