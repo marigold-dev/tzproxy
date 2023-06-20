2d3b8a2375d0abd7f8460024a7af7d1f2a7d8a8a
Felipe Cardozo <fraifelipe@gmail.com>
2023-06-19 22:42:15 -0300

    add prometheus metrics

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.20
 require (
 	github.com/coocood/freecache v1.2.3
 	github.com/fraidev/go-echo-cache v0.0.0-20230512221625-1c61b39e2dc0
-	github.com/labstack/echo/v4 v4.10.2 // direct
 	github.com/rs/zerolog v1.29.1
 	github.com/ulule/limiter/v3 v3.11.2 // direct
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
@@ -26,4 +25,21 @@ require (
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
 	golang.org/x/time v0.3.0 // indirect; direct
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
+
+require (
+	github.com/labstack/echo-contrib v0.15.0
+	github.com/labstack/echo/v4 v4.10.3-0.20230531055333-42f07ed88040
 )
--- main.go
+++ main.go
@@ -10,19 +10,22 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	middlewares "github.com/marigold-dev/tzproxy/middlewares"
+	"github.com/marigold-dev/tzproxy/middlewares"
 	utils "github.com/marigold-dev/tzproxy/utils"
 	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+	println(runtime.NumCPU() - 1)
 	config := utils.NewConfig()
 	store := memory.NewStore()
 
 	e := echo.New()
+
 	url, err := url.Parse(config.TezosHost)
 	if err != nil {
 		e.Logger.Fatal(err)
@@ -50,8 +53,10 @@ func main() {
 		Transport:  transport,
 	}
 
+	// Middlewares
 	e.Use(middleware.Recover())
 	e.Use(middleware.RequestLoggerWithConfig(config.RequestLoggerConfig))
+	e.Use(echoprometheus.NewMiddleware("proxy"))
 	e.Use(middlewares.CORS(config))
 	e.Use(middlewares.RateLimit(store, config))
 	e.Use(middlewares.BlockRoutes(config))
@@ -59,10 +64,20 @@ func main() {
 	e.Use(middlewares.Gzip(config))
 	e.Use(middleware.ProxyWithConfig(proxyConfig))
 
-	// Start server
+	// Start metrics server
+	eMetrics := echo.New()
+	eMetrics.GET("/metrics", echoprometheus.NewHandler())
+	go func() {
+		if err := eMetrics.Start(":9000"); err != nil && err != http.ErrServerClosed {
+			eMetrics.Logger.Fatal(err)
+		}
+	}()
+
+	// Start proxy
 	go func() {
 		if err := e.Start(config.Host); err != nil && err != http.ErrServerClosed {
 			e.Logger.Fatal("Shutting down the server")
+			e.Logger.Fatal(err)
 		}
 	}()
 
